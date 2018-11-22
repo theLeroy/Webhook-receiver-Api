3d73dae750ef1b74a86c3f62767549254f8a5cee
Leroy <4c65726f79@gmail.com>
2018-11-22 16:06:10 +0100

    clean

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -25,62 +25,6 @@ export const generateHash = (length = 64, casesensitive = true) => {
   return hash
 }
 
-export const newFlowFromCouponType = async (couponType) => {
-  const Flow = require('../models/Flow').default
-  const Video = require('../models/Video').default
-  const Question = require('../models/Question').default
-  const Answer = require('../models/Answer').default
-
-  let flow = new Flow({
-    couponType
-  })
-
-  let videoId = couponType.videos[Math.floor(Math.random() * couponType.videos.length)]
-  let video = await Video.findById(videoId)
-  if (!video) throw new Error(`Could not find video for couponType with id ${couponType._id}`)
-  flow.video = video
-
-  let questionId = video.questions[Math.floor(Math.random() * video.questions.length)]
-  let question = await Question.findById(questionId)
-  if (!question) throw new Error(`Could not find question for video with id ${video._id}`)
-  flow.question = question
-
-  if (question.correctAnswers.length === 0) throw new Error(`Could not find correct answers for question with id ${question._id}`)
-  let correctAnswerId = question.correctAnswers[Math.floor(Math.random() * question.correctAnswers.length)]
-  flow.answers.push(await Answer.findById(correctAnswerId))
-
-  if (question.wrongAnswers.length === 0) throw new Error(`Could not find wrong answers for question with id ${question._id}`)
-  let wrongAnswerIndexesUsed = []
-  for (let i = 0; i < 2; i++) {
-    let index
-    do {
-      index = Math.floor(Math.random() * question.wrongAnswers.length)
-    } while (wrongAnswerIndexesUsed.indexOf(index) !== -1)
-    wrongAnswerIndexesUsed.push(index)
-    let wrongAnswerId = question.wrongAnswers[index]
-    flow.answers.push(await Answer.findById(wrongAnswerId))
-  }
-
-  flow.answers.shuffle()
-
-  couponType.timesUsedInStory++
-  couponType.save()
-  video.timesUsedInStory++
-  video.save()
-  question.timesUsedInStory++
-  question.save()
-  for (let i = 0; i < flow.answers.length; i++) {
-    flow.answers[i].timesUsedInStory++
-    flow.answers[i].save()
-  }
-
-  return flow
-}
-
-export const sendSmsForStory = async (story) => {
-
-}
-
 export const initDb = async () => {
   // The following line should be deleted once the production initializer is done
   await initMockDb()
